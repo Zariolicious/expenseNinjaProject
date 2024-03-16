60c71a64cfc88be556316362be63daa5f45ebc53
Zariolicious <zendel2040@gmail.com>
2024-03-15 23:23:45 -0500

    Reordered GUI to improve functionality, introduced feature letting user submit a balance for financing purposes

--- main.py
+++ main.py
@@ -11,13 +11,23 @@ data = Database(db='myexpense.db') #db is parameter from db class (name must hav
 count = 0 
 selected_rowid = 0 
 
+def submit_balance():
+    global desired_balance #declate as global to use it later
+    desired_balance = float(balance_entry.get())
+    try:
+        desired_balance = float(desired_balance) #converts input to float
+        messagebox.showinfo("Success", f"Desired balance set to: {desired_balance}")
+        prompt_window.destroy() #closes window after submit
+    except ValueError:
+        messagebox.showerror("Error", "Please enter a number")
+
 def saveRecord():
     global data
     data.insertRecord(item_name=item_name.get(), item_price=item_amt.get(), purchase_date=transaction_date.get())
 
 def setDate():
     date = dt.datetime.now()
-    dopvar.set(f'{date:%d %B %Y}') #dopvar is the widget where the current date is displayed
+    dopvar.set(f'{date:%d %B %Y}') #date of purchase is the widget where the current date is displayed
 
 def clearEntries():
     item_name.delete(0,'end')
@@ -58,6 +68,7 @@ def update_record():
         tv.item(selected, text="", values=(namevar.get(), amtvar.get(), dopvar.get()))
     except Exception as ep:
         messagebox.showerror('Error', ep)
+    
     item_name.delete(0, END)
     item_amt.delete(0, END)
     transaction_date.delete(0, END)
@@ -68,12 +79,16 @@ def totalBalance():
     f = data.fetchRecord(query="Select sum(item_price) from expense_record")
     for i in f: 
         for j in i:
-            messagebox.showinfo('Current Balance: ', f"Total Expense: ' {j} \nBalance Remaining: {5000 - j}") #change 5000 to suit your needs
+            if j is not None:
+                remaining_balance = 5000 - j
+                messagebox.showinfo('Current Balance: ', f"Total Expense: ' {j} \nBalance Remaining: {remaining_balance}") 
+            else: 
+                messagebox.showinfo('Current Balance: ', f"Total Expense: 0 \nDesired Balance: {desired_balance}")
 
 def refreshData():
     for item in tv.get_children():
         tv.delete(item)
-    fetch_Records() #displays items
+    fetchRecords() #displays items
 
 def deleteRow():
     global selected_rowid
@@ -85,15 +100,41 @@ def deleteRow():
 ws = Tk()
 ws.title('Expense Ninja')
 
+prompt_window = Toplevel(ws)
+prompt_window.title('Balance Setting')
+
+prompt_frame = Frame(
+    prompt_window, 
+    padx=10, 
+    pady=10)
+prompt_frame.pack()
+
+balance_label = Label(prompt_frame, text="Enter your desired balance: ")
+balance_label.grid(row=0, column=0, sticky=W)
+
+balance_entry = Entry(prompt_frame)
+balance_entry.grid(row=0, column=1)
+
+
 #font 
 f = ('Times new roman', 14,)
 namevar = StringVar()
 amtvar = IntVar()
 dopvar = StringVar()
 
-ws.mainloop()
+#Button to submit balance
 
-# Frame widget / display of data
+submit_button = Button(
+    prompt_frame, 
+    text ="Submit",
+    font=f,
+    bg = '#1F51FF', 
+    command=submit_balance,
+    width=15
+)
+
+
+# Frame / display of data
 f2 = Frame(ws)
 f2.pack()
 
@@ -105,62 +146,7 @@ f1 = Frame(
 )
 f1.pack(expand=True, fill=BOTH)
 
-#Label widget
-Label(f1, text = "ITEM NAME", font=f).grid(row=0, column=0, sticky=W)
-Label(f1, text = "ITEM PRICE", font=f).grid(row=1, column=0, sticky=W)
-Label(f1, text = "PURCHASE DATE", font=f).grid(row=2, column=0, sticky=W)
-
-#entry widgets 
-item_name = Entry(f1, font=1)
-item_amt = Entry(f1, font=f, textvariable=amtvar)
-transaction_date = Entry(f1, font=f, textvariable=dopvar)
-
-#entry grid placement
-item_name.grid(row=0, column=1, sticky=EW, padx=(10, 0))
-item_amt.grid(row=1, column=1, sticky=EW, padx=(10, 0))
-transaction_date.grid(row=2, column=1, sticky=EW, padx=(10, 0))
-
-#button grid placement
-
-cur_date.grid(row=3, column=1, sticky=EW, padx=(10, 0))
-submit_btn.grid(row=0, column=2, sticky=EW, padx=(10, 0))
-clr_btn.grid(row=1, column=2, sticky=EW, padx=(10, 0))
-quit_btn.grid(row=2, column=2, sticky=EW, padx=(10, 0))
-total_bal.grid(row=0, column=3, sticky=EW, padx=(10, 0))
-update_btn.grid(row=1, column=3, sticky=EW, padx=(10, 0))
-del_btn.grid(row=2, column=3, sticky=EW, padx=(10, 0))
-
-#viewing widget
-tv = ttk.Treeview(f2, columns=(1, 2, 3, 4), show ='deadings', height=8)
-tv.pack(side="left")
-
-#treeview headings
-tv.column(1, anchor=CENTER, stretch=NO, width=70)
-tv.column(2, anchor=CENTER)
-tv.column(3, anchor=CENTER)
-tv.column(4, anchor=CENTER)
-tv.heading(1, text="Serial no")
-tv.heading(2, text="Item Name", )
-tv.heading(3, text="Item Price")
-tv.heading(4, text="Purchase Date")
-
-#binding treeview
-tv.bind("<ButtonRelease-1>", select_record)
-
-#treeview style
-style = ttk.Style()
-style.theme_use("default")
-style.map("Treeview")
-
-#scrollbar widget
-scrollbar = Scrollbar(f2, orient='vertical')
-scrollbar.configure(command=tv.yview)
-scrollbar.pack(side="right", fill="y")
-tv.config(yscrollcommand=scrollbar.set)
-
-#infinit loop
-ws.mainloop()
-
+#
 #Action buttons
 cur_date = Button(
     f1, 
@@ -222,3 +208,61 @@ del_btn = Button(
     command=deleteRow,
     font=f
  )
+
+#Label widget
+Label(f1, text = "ITEM NAME", font=f).grid(row=0, column=0, sticky=W)
+Label(f1, text = "ITEM PRICE", font=f).grid(row=1, column=0, sticky=W)
+Label(f1, text = "PURCHASE DATE", font=f).grid(row=2, column=0, sticky=W)
+
+#entry widgets 
+item_name = Entry(f1, font=1, textvariable=namevar)
+item_amt = Entry(f1, font=f, textvariable=amtvar)
+transaction_date = Entry(f1, font=f, textvariable=dopvar)
+
+#entry grid placement
+item_name.grid(row=0, column=1, sticky=EW, padx=(10, 0))
+item_amt.grid(row=1, column=1, sticky=EW, padx=(10, 0))
+transaction_date.grid(row=2, column=1, sticky=EW, padx=(10, 0))
+
+
+#viewing widget
+tv = ttk.Treeview(f2, columns=(1, 2, 3, 4), show ='headings', height=8)
+tv.pack(side="left")
+
+#treeview headings
+tv.column(1, anchor=CENTER, stretch=NO, width=70)
+tv.column(2, anchor=CENTER)
+tv.column(3, anchor=CENTER)
+tv.column(4, anchor=CENTER)
+tv.heading(1, text="Serial no")
+tv.heading(2, text="Item Name", )
+tv.heading(3, text="Item Price")
+tv.heading(4, text="Purchase Date")
+
+#binding treeview
+tv.bind("<ButtonRelease-1>", select_record)
+
+#treeview style
+style = ttk.Style()
+style.theme_use("default")
+style.map("Treeview")
+
+#scrollbar widget
+scrollbar = Scrollbar(f2, orient='vertical')
+scrollbar.configure(command=tv.yview)
+scrollbar.pack(side="right", fill="y")
+tv.config(yscrollcommand=scrollbar.set)
+
+#button grid placement
+
+cur_date.grid(row=3, column=1, sticky=EW, padx=(10, 0))
+submit_btn.grid(row=0, column=2, sticky=EW, padx=(10, 0))
+clr_btn.grid(row=1, column=2, sticky=EW, padx=(10, 0))
+quit_btn.grid(row=2, column=2, sticky=EW, padx=(10, 0))
+total_bal.grid(row=0, column=3, sticky=EW, padx=(10, 0))
+update_btn.grid(row=1, column=3, sticky=EW, padx=(10, 0))
+del_btn.grid(row=2, column=3, sticky=EW, padx=(10, 0))
+submit_button.grid(row=1, column=0, columnspan=2, pady=10) #pf frame
+
+
+ws.mainloop()
